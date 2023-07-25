828e5eb3e15ccc6d28859ea2ad3cf81a34d615f6
tonthatminhnhat <toilaminhnhat01@gmail.com>
2023-07-25 15:23:35 +0700

    FluterwebDashboard

--- lib/layout.dart
+++ lib/layout.dart
@@ -1,18 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:web_dashboard_flutter/widgets/large_screen.dart';
 import 'package:web_dashboard_flutter/widgets/small_screen.dart';
+import 'package:web_dashboard_flutter/widgets/top_nav.dart';
 import 'helpers/responsiveness.dart';
 
 class SiteLayout extends StatelessWidget {
-  const SiteLayout({ Key? key }) : super(key: key);
-
+final GlobalKey<ScaffoldState> scaffoldKey=GlobalKey();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
-      ),
+      key:scaffoldKey,
+      appBar:  topNavigationBar(context, scaffoldKey),
+      drawer: Drawer(),
       body: ResponsiveWidget(largeScreen: LargeScreen(),smallScreen: SmallScreen(), mediumScreen: null,),
 
     );
